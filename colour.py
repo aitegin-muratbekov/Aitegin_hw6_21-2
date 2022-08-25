709d4faf7a1ce85b38055374f89a43629e7a179e
aitegin-muratbekov <65114143+aitegin-muratbekov@users.noreply.github.com>
2022-08-25 17:35:44 +0600

    module colour is updated

--- colour.py
+++ colour.py
@@ -4,5 +4,5 @@ def add_colour_content():
     with open('MOCK_DATA.txt', 'r', encoding='utf-8') as file:
         content = file.read()
     with open('colour.txt', 'w', encoding='utf-8') as file:
-        file.write("\n".join(re.findall(r'#[\w]{6}', content)))
+        file.write("\n".join(re.findall(r'#[a-zA-Z0-9]{6}', content)))
 
